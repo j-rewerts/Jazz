9a35b8c3150f76954f806b87b7c73e5d60d46dc0
Jared Rewerts <jaredrewerts@gmail.com>
2017-11-28 14:16:51 -0700

    Initialized a universal iOS app.

---
+++ vip-chat-native-ios/vip-chat-native-ios/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  vip-chat-native-ios
+//
+//  Created by Jared Rewerts on 11/28/17.
+//  Copyright © 2017 City of Edmonton. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
