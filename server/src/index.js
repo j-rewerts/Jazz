b4bde6df6428ae4674305550a36536da93a7e29a
Jared Rewerts <jaredrewerts@gmail.com>
2019-03-20 10:19:39 -0600

    Require LiveChat license on start.

--- server/src/index.js
+++ server/src/index.js
@@ -2,6 +2,10 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT || 3000
 
+if (!process.env.LC_LICENSE) {
+  throw new Error('Please provide your LiveChat license number in LC_LICENSE')
+}
+
 app.get('/', (req, res) => res.send('Hello World!'))
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
